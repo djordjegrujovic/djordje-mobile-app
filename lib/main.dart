b6e011ee8135e4e40b764859113135dce7c1fcaf
Djordje Grujovic <djgrujovic13@gmail.com>
2021-10-25 12:10:11 +0200

    Navigator 2.0 added

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,93 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _selectedUser;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      /// Remove debug banner
-      debugShowCheckedModeBanner: false,
-
-      /// Title of app
-      title: 'Mobile app',
-
-      /// Theme settings
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blue,
+      home: Navigator(
+        pages: [
+          MaterialPage(child: UsersView(
+            didSelectUser: (user) {
+              setState(() => _selectedUser = user);
+            },
+          )),
+          if (_selectedUser != null)
+            MaterialPage(child: UsersDetailView(user: _selectedUser))
+        ],
+        onPopPage: (route, result) {
+          _selectedUser = '';
+          return route.didPop(result);
+        },
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.light,
+    );
+  }
+}
+
+class UsersView extends StatelessWidget {
+  final ValueChanged didSelectUser;
+
+  const UsersView({Key? key, required this.didSelectUser}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _users = ["Pera", "Mika", "Zika", "Laza"];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User list'),
       ),
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+          child: ListView.builder(
+            itemCount: _users.length,
+            itemBuilder: (context, index) {
+              final user = _users[index];
 
-      localizationsDelegates: [
-        // GlobalMaterialLocalizations.delegate,
-        // GlobalWidgetsLocalizations.delegate,
-        // GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('de', 'DE'),
-      ],
-
-      /// Content
-      home: const Scaffold(
-        body: Center(
-          child: Text('Home screen'),
+              return Container(
+                margin: const EdgeInsets.only(bottom: 10.0),
+                child: Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: ListTile(
+                      title: Text(user),
+                      onTap: () => didSelectUser(user),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class UsersDetailView extends StatelessWidget {
+  final String user;
+
+  const UsersDetailView({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User details'),
+      ),
+      body: Center(
+        child: Text('Hello, $user'),
       ),
     );
   }
